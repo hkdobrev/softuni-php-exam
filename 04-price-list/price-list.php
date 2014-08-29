469b86e073fa1ae83f5a12bc47bca534b3c2cbd0
Haralan Dobrev <hkdobrev@gmail.com>
2014-08-29 19:57:36 +0300

    Add initial solution for task 04 - price list

---
+++ 04-price-list/price-list.php
@@ -0,0 +1,60 @@
+<?php
+
+$html = $_GET['priceList'];
+
+define('PRODUCT', 0);
+define('CATEGORY', 1);
+define('PRICE', 2);
+define('CURRENCY', 3);
+
+$indexes = [
+	PRODUCT  => 'product',
+	CATEGORY => 'category',
+	PRICE    => 'price',
+	CURRENCY => 'currency',
+];
+
+$doc = new DOMDocument('1.0', 'UTF-8');
+libxml_use_internal_errors(true);
+$doc->loadHTML($html);
+libxml_clear_errors();
+
+$products = [];
+
+$xpath = new DOMXPath($doc);
+
+$rows = $xpath->query('//tr');
+
+$isHeaderPassed = false;
+
+foreach ($rows as $row) {
+	if (!$isHeaderPassed) {
+		$isHeaderPassed = true;
+		continue;
+	}
+	$product = [];
+	$cells = $row->childNodes;
+	$index = 0;
+	foreach ($cells as $cell) {
+		if (isset($cell->nodeName) and $cell->nodeName === 'td') {
+			$product[$indexes[$index]] = html_entity_decode(trim($cell->nodeValue), ENT_COMPAT | ENT_HTML401, 'UTF-8');
+			$index++;
+		}
+	}
+
+	if ($product) {
+		$category = $product['category'];
+		unset($product['category']);
+		$products [$category][] = $product;
+	}
+}
+
+ksort($products);
+
+foreach ($products as $category => &$categoryProducts) {
+	usort($categoryProducts, function($a, $b) {
+		return strcmp($a['product'], $b['product']);
+	});
+}
+
+echo json_encode($products);
